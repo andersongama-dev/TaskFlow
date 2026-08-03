26e762a5bf6dfee34f8720eae7b2cdef4dc04a73
Anderson Gama <andersongamasilva5@gmail.com>
2026-08-03 14:44:38 -0300

    feat: add home screen and contact registration

---
+++ app/src/main/java/com/nyz/taskflow/NewContactActivity.kt
@@ -0,0 +1,12 @@
+package com.nyz.taskflow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_contact)
+    }
+}
